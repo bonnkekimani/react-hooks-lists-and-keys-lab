cad7877d3245f239b80093d71ff6b531690ea9cd
bonnkekimani <bonnke.kimani@student.moringaschool.com>
2023-04-24 09:04:46 +0300

    nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
-}
+  return <nav>{/* display an <a> tag for each link here */  <nav>  {links.map((link, index)=>(  <a key={index} href={`#${link}`}>{link}</a>  ))}  </nav>
+  }</nav>;}
 
 export default NavBar;
